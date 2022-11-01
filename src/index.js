54fc69c0cddb235b87778e2f80c2a589384842b0
Wolwer1ne <wolwerine@yandex.ru>
2022-11-01 15:49:34 +0300

    Add map

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import {SpaceX} from "./api/spacex";
+import * as d3 from "d3";
+import * as Geo from './geo.json'
+
 document.addEventListener("DOMContentLoaded", setup)
 
 function setup(){
@@ -6,6 +9,7 @@ function setup(){
     spaceX.launches().then(data=>{
         const listContainer = document.getElementById("listContainer")
         RenderLaunches(data, listContainer);
+        drawMap();
     })
 }
 function RenderLaunches(launches, container){
@@ -18,3 +22,31 @@ function RenderLaunches(launches, container){
     container.replaceChildren(list);
 }
 
+function drawMap(){
+    const width = 640;
+    const height = 480;
+    const margin = {top: 20, right: 10, bottom: 40, left: 100};
+    const svg = d3.select('#map').append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+    const projection = d3.geoMercator()
+        .scale(70)
+        .center([0,20])
+        .translate([width / 2 - margin.left, height / 2]);
+    svg.append("g")
+        .selectAll("path")
+        .data(Geo.features)
+        .enter()
+        .append("path")
+        .attr("class", "topo")
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        .attr("fill", function (d) {
+            return colorScale(0);
+        })
+        .style("opacity", .7)
+}
