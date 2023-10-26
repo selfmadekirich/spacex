fe8a7d136c3746f5a827bf93cfa5176fdb00c259
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2023-10-26 22:58:37 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ import {SpaceX} from "./api/spacex";
 import * as d3 from "d3";
 import * as Geo from './geo.json'
 
+
 document.addEventListener("DOMContentLoaded", setup)
+
+const mp = {}
 
 function setup(){
     const spaceX = new SpaceX();
     spaceX.launches().then(data=>{
         const listContainer = document.getElementById("listContainer")
         renderLaunches(data, listContainer);
-        drawMap();
+    })
+
+    spaceX.launchpads().then(data => {
+        console.log(data)
+        const d = {
+            "ids":[],
+            "features": []};
+            data.forEach(launchpad => {
+                d.features.push({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [launchpad.longitude, launchpad.latitude ]
+            }});
+            d.ids.push(launchpad.id)
+        })
+        
+        drawMap(d);
     })
 }
 function renderLaunches(launches, container){
@@ -17,12 +37,29 @@ function renderLaunches(launches, container){
     launches.forEach(launch=>{
         const item = document.createElement("li");
         item.innerHTML = launch.name;
+        item.id = launch.id
+        mp[launch.id] = launch.launchpad
+        item.onmouseover = liItemOnMouseHover
+        item.onmouseleave = liItemOnMouseLeave
         list.appendChild(item);
     })
     container.replaceChildren(list);
 }
 
-function drawMap(){
+function liItemOnMouseHover(){
+    launchpadId = mp[this.id]
+    launchpad = document.getElementById(launchpadId)
+    launchpad.setAttribute('fill','yellow')
+}
+
+
+function liItemOnMouseLeave(){
+    launchpadId = mp[this.id]
+    launchpad = document.getElementById(launchpadId)
+    launchpad.setAttribute('fill','red')
+}
+
+function drawMap(data){
     const width = 640;
     const height = 480;
     const margin = {top: 20, right: 10, bottom: 40, left: 100};
@@ -44,9 +81,23 @@ function drawMap(){
         .attr("class", "topo")
         .attr("d", d3.geoPath()
             .projection(projection)
-        )
-        .attr("fill", function (d) {
-            return colorScale(0);
+        ).attr("fill", function (d) {
+            return 'green';
         })
         .style("opacity", .7)
+    
+    svg.selectAll('.dots')
+        .data(data.features)
+        .enter()
+        .append('path')
+        .attr('d',d3.geoPath()
+        .projection(projection))
+        .attr('class', 'dots')
+        .attr('fill','red')
+
+    const element  = document.querySelectorAll('.dots');
+    console.log(element)
+        element.forEach((e, i) => {
+            e.setAttribute("id", data.ids[i]);
+        });
 }
